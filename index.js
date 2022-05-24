a08e931228ee09d30de671d93e3290da5910b757
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-24 09:39:27 -0400

    Node test

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const http = require(http).Server(app);
+
+app.listen('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+var server = http.listen(3000, () => {
+    console.log('Server listening on port ',server.address().port)
+});
